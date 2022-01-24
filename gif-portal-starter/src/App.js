194c695afa4920f716aa2dfe79afb2587f702f0a
MimirTheGiant <mimirthegiant20@gmail.com>
2022-01-24 01:36:45 +0100

    fix keypair2

--- gif-portal-starter/src/App.js
+++ gif-portal-starter/src/App.js
@@ -5,6 +5,7 @@ import { Program, Provider, web3 } from '@project-serum/anchor';
 
 import idl from './idl.json';
 import './App.css';
+import kp from './keypair.json'
 
 
 
@@ -14,8 +15,12 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 // SystemProgram is a reference to the Solana runtime!
 const { SystemProgram, Keypair } = web3;
+
 // Create a keypair for the account that will hold the GIF data.
-let baseAccount = Keypair.generate();
+const arr = Object.values(kp._keypair.secretKey)
+const secret = new Uint8Array(arr)
+const baseAccount = web3.Keypair.fromSecretKey(secret)
+
 // Get our program's id from the IDL file.
 const programID = new PublicKey(idl.metadata.address);
 
